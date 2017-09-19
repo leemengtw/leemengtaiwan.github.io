56c460a9ad13467083b433ab4af515877ed3c363
leemeng <leemeng@marketenterprise.co.jp>
2017-09-19 21:17:24 +0900

    Update documentation

--- output/theme/js/jquery.captions.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	// Every image referenced from a Markdown document
-	$("article img:visible").each(function() {
-		// Let's put a caption if there is one
-		if($(this).attr("title"))
-			$(this).wrap('<figure class="image"></figure>')
-				.after('<figcaption>'+$(this).attr("title")+'</figcaption>');
-		});
-});
