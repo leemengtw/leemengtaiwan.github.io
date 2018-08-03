62d85504b1358c06a91de73409d1681416bc2c2f
Meng Lee <b98705001@gmail.com>
2018-08-04 00:12:42 +0900

    Update Google Page View

---
+++ theme/js/scroll-detect.js
@@ -0,0 +1,11 @@
+// References: https://codepen.io/lehollandaisvolant/pen/ryrrGx
+var scrollPos = 0;
+var hdr = $('header');
+window.addEventListener('scroll', function(){
+  // detects new state and compares it with the new one
+  if ((document.body.getBoundingClientRect()).top > scrollPos)
+    hdr.addClass('scrolling');
+  else
+    hdr.removeClass('scrolling');
+  scrollPos = (document.body.getBoundingClientRect()).top;
+});
