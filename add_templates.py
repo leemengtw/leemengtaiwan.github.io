d8ff766c3b4f2bd81a10f6df0de17df990618688
Meng Lee <b98705001@gmail.com>
2018-08-31 21:44:29 +0900

    Add project as submodule

--- add_templates.py
+++ add_templates.py
@@ -1,5 +1,7 @@
 import os
 from shutil import copyfile
+import glob
+
 
 def main():
 
@@ -10,7 +12,27 @@ def main():
     for template in templates:
         copyfile('./{}'.format(template), './output/{}'.format(template))
 
-    print('Done: Processed {} templates.'.format(len(templates)))
+    print('Done: Processed {} additional templates.'.format(len(templates)))
+
+    # add ipynb from other projects
+    extension = 'ipynb'
+    source_dir = './projects'
+    destination_dir = './content/projects'
+    num_files_copied = 0
+
+    ipynb_paths = glob.iglob(os.path.join(source_dir, '**', '*.%s' % extension), recursive=True)
+    for ipynb_p in ipynb_paths:
+        destination_p = ipynb_p.replace(source_dir, destination_dir)
+        # create new project folder for the ipynb file in content folder if not exist
+        sub_destination_dir = '/'.join(destination_p.split('/')[:-1])
+        if not os.path.exists(sub_destination_dir):
+            os.makedirs(sub_destination_dir)
+
+        copyfile(ipynb_p, destination_p)
+        num_files_copied += 1
+
+    print('Done: Processed {} additional ipynb files from other projects.'.format(num_files_copied))
+
 
 if __name__ == "__main__":
     main()
