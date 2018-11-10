54f4b9fbd9d37294ca458ca8fa1aab2bae9d63fd
Meng Lee <b98705001@gmail.com>
2018-11-10 22:39:32 +0900

    add_new_book

--- theme/js/scroll-detect.js
+++ theme/js/scroll-detect.js
@@ -3,9 +3,11 @@ var scrollPos = 0;
 var hdr = $('header');
 window.addEventListener('scroll', function(){
   // detects new state and compares it with the new one
-  if ((document.body.getBoundingClientRect()).top > scrollPos)
-    hdr.addClass('scrolling');
-  else
-    hdr.removeClass('scrolling');
+  if ((document.body.getBoundingClientRect()).top > scrollPos) {
+     hdr.addClass('scrolling');
+  }
+  else {
+     hdr.removeClass('scrolling');
+  }
   scrollPos = (document.body.getBoundingClientRect()).top;
 });
